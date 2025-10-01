f204b72d03d47971d20bab886c3dfc09d2f148ee
AndreyDeimos <and.mi.eremenko@gmail.com>
2025-10-01 10:57:16 +0000

    transparency

--- init.lua
+++ init.lua
@@ -28,6 +28,7 @@ vim.pack.add({
 
 	{ src = "https://github.com/v1nh1shungry/cppman.nvim" },
 	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
+	{ src = "https://github.com/xiyaowong/transparent.nvim" },
 })
 
 -- options
